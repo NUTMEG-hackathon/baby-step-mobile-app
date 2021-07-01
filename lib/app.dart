9efb09b54c4f4232f930766908089e68b7faddd5
Toshichika Mashimo <mashita1023@gmail.com>
2021-07-02 07:15:33 +0900

    [add] cupertino tab scafold

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:baby_step_up_app/providers/providers.dart';
 
-import 'pages/items_page/items_page.dart';
+//import 'pages/items_page/items_page.dart';
+import 'widgets/bottom_navigation_bar.dart';
 
 class App extends ConsumerWidget {
   const App({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       navigatorKey: ref.watch(navigatorKeyProvider),
-      home: const ItemsPage(),
+      home: BottomTabBar()
     );
   }
 }
